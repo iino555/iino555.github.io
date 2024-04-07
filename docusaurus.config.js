731c79464ba6f8f21a951dffaecc9a95580e5a37
iino555 <18608400775@163.com>
2024-04-08 01:28:25 +0800

    modify

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   trailingSlash: false,
-  deploymentBranch: "main",
+  deploymentBranch: "gh-pages",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
